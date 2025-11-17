a8ac4a2b870a4a4a3d1ba477163959f6c6c200fa
iceamonwat09 <100498315+iceamonwat09@users.noreply.github.com>
2025-11-17 10:56:56 +0700

    Edit  ไม่บันทึก เวลา กับชั่วโมงใหม่ บันทึกเป็น 0
    ยังติด หน้า  Edit  ไม่บันทึก เวลา กับชั่วโมงใหม่ บันทึกเป็น 0

---
+++ Views/Views/Home/MonthlyRequests.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrainingRequestApp.Views.Home
+{
+    public class MonthlyRequestsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
